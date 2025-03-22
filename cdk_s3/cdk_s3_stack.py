0ad679da9aa7829f3d962863b48b22fe440a9771
x22156461 <x22156461@student.ncirl.ie>
2025-03-22 16:43:23 +0000

    first commit

--- cdk_s3/cdk_s3_stack.py
+++ cdk_s3/cdk_s3_stack.py
@@ -1,6 +1,7 @@
 from aws_cdk import (
     # Duration,
     Stack,
+    aws_s3 as s3
     # aws_sqs as sqs,
 )
 from constructs import Construct
@@ -17,3 +18,13 @@ class CdkS3Stack(Stack):
         #     self, "CdkS3Queue",
         #     visibility_timeout=Duration.seconds(300),
         # )
+
+        bucket = s3.Bucket(
+            self,
+            "MyS3Bucket",
+            bucket_name="test-bucket",
+            versioned=True,
+            encryption=s3.BucketEncryption.S3_MANAGED,
+            block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
+            removal_policy=RemovalPolicy.DESTROY
+        )
